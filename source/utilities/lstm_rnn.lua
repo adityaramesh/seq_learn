5b5c304e962b775ce30122c18917b68f7e6c3174
Aditya Ramesh <_@adityaramesh.com>
2015-05-08 06:04:58 -0400

    Finished forward and backward implementations.

--- source/utilities/lstm_rnn.lua
+++ source/utilities/lstm_rnn.lua
@@ -1,4 +1,7 @@
 require "torch"
+require "cunn"
+require "nngraph"
+local LookupTable = nn.LookupTable
 
 LSTM_RNN = {}
 LSTM_RNN.__index = LSTM_RNN
@@ -127,13 +130,19 @@ function LSTM_RNN.create(params)
 	local model = {
 		length = params.length,
 		layers = params.layers,
+		batch_size = params.batch_size,
 		network = module,
 		criterion = nn.ClassNLLCriterion():cuda(),
 
-		-- Used for the previous state vector supplied to the first
-		-- clone during the forward pass, and for the state gradients
-		-- supplied to the last clone during the backward pass.
-		zero_state = torch.zeros(params.batch_size,
+		-- Used to keep track of which tokens determine the end of
+		-- documents in the current window. Important for figuring out
+		-- when to use a new state instead of the previous one, or when
+		-- to use a zero input gradient instead of the next one.
+		token_ends_doc = torch.ByteTensor(params.length, params.batch_size):zero(),
+		last_doc_endings = torch.IntTensor(params.batch_size):zero(),
+
+		-- A useful constant of which we will make many shallow copies.
+		zero_layer_state = torch.zeros(params.batch_size,
 			params.lstm_cell_width):cuda(),
 
 		-- The predictions of the network are obtained using a dummy
@@ -145,12 +154,40 @@ function LSTM_RNN.create(params)
 
 		preds = {},
 		states = {},
-		state_grads = {},
-		zero_state_grads = {}
+
+		-- The states supplied to the first clone during the forward
+		-- pass.
+		zero_state = {},
+		-- Used when the current token along any of the `batch_size`
+		-- components of the input is the beginning of a document. Then
+		-- the next state will be a mixture of zeros and previous
+		-- states.
+		prev_states = {},
+		-- The state gradients supplied to the last clone during the
+		-- backward pass.
+		zero_state_grads = {},
+		-- Used when the current token along any of the `batch_size`
+		-- componenst of the input is the end of a document. Then the
+		-- state gradients will be a mixture of zeros and state
+		-- gradients from the next clone.
+		state_grad_buf = {},
+		next_state_grads = {}
 	}
 
 	for i = 1, 2 * model.layers do
+		model.zero_state[i] = model.zero_state
+	end
+
+	for i = 2, model.length do
+		model.prev_states[i] = {}
+		for j = 1, 2 * model.layers do
+			model.prev_states[i][j] = model.zero_state:clone()
+		end
+	end
+
+	for i = 1, 2 * model.layers do
 		model.zero_state_grads[i] = model.zero_state
+		model.state_grad_buf[i] = model.zero_state:clone()
 	end
 	return model
 end
@@ -162,41 +199,90 @@ function LSTM_RNN:restore()
 	self.clones = clone_network(self.module, self.length)
 end
 
-function LSTM_RNN:forward(i, input, output)
+function LSTM_RNN:forward(i, input, output, token_ends_doc)
 	assert(i >= 1 and i <= self.length)
-	assert(input:size(1) == output:size(1))
+	assert(input:size(1) == params.batch_size)
+	assert(output:size(1) == params.batch_size)
+	assert(token_ends_doc:size(1) == params.batch_size)
 
-	local cur_state = {}
-	local prev_state = i == 1 and self.zero_state or self.states[i - 1]
-	self.states[i], self.preds[i] = unpack(self.clones[i]:forward({input, s}))
+	self.token_ends_doc[i]:copy(token_ends_doc)
+	if i == 1 then
+		self.last_doc_endings:zero()
+	end
+	for j = 1, token_ends_doc:size(1) do
+		if token_ends_doc[j] == 1 then
+			self.last_doc_endings[j] = i
+		end
+	end
+
+	local prev_state
+	if i == 1 then
+		prev_state = self.zero_state
+	else if self.token_ends_doc[i - 1]:sum() == 0 then
+		prev_state = self.states[i - 1]
+	else
+		prev_state = self.prev_state[i]
+		for j = 1, 2 * model:layers() do
+			prev_state[j]:copy(self.states[i - 1][j])
+		end
+		for j = 1, self.token_ends_doc:size(2) do
+			if self.token_ends_doc[i - 1][j] == 1 then
+				for k = 1, 2 * model:layers() do
+					prev_state[k][j]:zero()
+				end
+			end
+		end
+	end
+
+	self.states[i], self.preds[i] = unpack(self.clones[i]:forward(
+		{input, prev_state}))
 
 	if output == nil then return end
 	return self.criterion:forward(self.preds[i], output)
 end
 
---
--- The `mask` parameter is used to nullify the gradient contributions of certain
--- components of the network in batch mode.
---
-function LSTM_RNN:backward(i, input, output, mask)
+function LSTM_RNN:backward(i, input, output)
 	assert(i >= 1 and i <= self.length)
-	assert(input:size(1) == output:size(1))
+	assert(input:size(1) == params.batch_size)
+	assert(output:size(1) == params.batch_size)
 
 	local output_grads = self.criterion:backward(self.preds[i], output)
-	if mask ~= nil then
-		assert(mask:size(1) == input:size(1))
-		for i = 1, #mask do
-			if mask[i] == 0 then
-				output_grads[i]:zero()
+	for j = 1, self.last_doc_endings:size(1) do
+		if i > self.last_doc_endings[j] then
+			output_grads[j]:zero()
+		end
+	end
+
+	local prev_state
+	if i == 1 then
+		prev_state = self.zero_state
+	else if self.token_ends_doc[i - 1]:sum() == 0 then
+		prev_state = self.states[i - 1]
+	else
+		prev_state = self.prev_state[i]
+	end
+
+	local next_state_grads
+	if i == self.batch_size then
+		next_state_grads = self.zero_state_grads
+	else if self.token_ends_doc[i]:sum() == 0 then
+		next_state_grads = self.next_state_grads
+	else
+		next_state_grads = self.state_grad_buf
+		for j = 1, 2 * model:layers() do
+			next_state_grads[j]:copy(self.next_state_grads[j])
+		end
+		for j = 1, self.token_ends_doc:size(2) do
+			if self.token_ends_doc[i][j] == 1 then
+				for k = 1, 2 * model:layers() do
+					next_state_grads[k][j]:zero()
+				end
 			end
 		end
 	end
 
-	local prev_state = i == 1 and self.zero_state or self.states[i - 1]
-	local prev_state_grads = i == self.length and self.zero_state_grads or
-		self.state_grads
 	self.state_grads = self.clones[i]:backward({input, prev_state},
-		{output_grads, prev_state_grads, self.pred_grads})[2]
+		{output_grads, next_state_grads, self.pred_grads})[2]
 end
 
 --
